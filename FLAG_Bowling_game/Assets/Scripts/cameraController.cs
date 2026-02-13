652a8a03480811e81dbe2a298fe55b7289b014e9
Deanosaureee <hayden.screechdean@gmail.com>
2026-02-13 17:44:30 +0000

    player and basic camera setup
    very janky, very temporary, but it's something

---
+++ FLAG_Bowling_game/Assets/Scripts/cameraController.cs
@@ -0,0 +1,52 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class cameraController : MonoBehaviour
+{
+    public enum enCameraPanDirections { none, up, down, left, right };
+
+    Camera thisCamera;
+
+    private Vector3 screenLocation = new Vector3(-35, 0, 0);
+    private Vector3 ballsLocationne = new Vector3(0, -90, 0);
+    private Vector3 laneLocation =new Vector3(0,0,0);
+    
+
+
+    void Start()
+    {
+        thisCamera = GetComponentInChildren<Camera>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   
+
+    public void PanCamera(enCameraPanDirections direction)
+    {
+        switch (direction) 
+        { 
+            case enCameraPanDirections.none:
+                break;
+            case enCameraPanDirections.up:
+                thisCamera.transform.rotation = Quaternion.Euler(screenLocation);
+                break;
+            case enCameraPanDirections.down:
+                thisCamera.transform.rotation = Quaternion.Euler(laneLocation);
+                break;
+            case enCameraPanDirections.left:
+                thisCamera.transform.rotation = Quaternion.Euler(ballsLocationne);
+                break;
+            case enCameraPanDirections.right:
+                thisCamera.transform.rotation = Quaternion.Euler(laneLocation);
+                break;
+        }
+    }
+
+}
---
+++ FLAG_Bowling_game/Assets/Scripts/inputManager.cs
@@ -0,0 +1,70 @@
+using System.Buffers;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static cameraController;
+
+public class inputManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    PlayerInput playerInputs;
+    InputAction interactAction;
+    InputAction panAction;
+    InputAction lookAction;
+    cameraController cameraControl;
+    void Start()
+    {
+        playerInputs = GetComponent<PlayerInput>();
+        cameraControl = GetComponent<cameraController>();
+
+        lookAction = playerInputs.actions["look"];
+        interactAction = playerInputs.actions["interact"];
+        panAction = playerInputs.actions["pan"];
+
+
+        lookAction.performed += GetInputs;
+        interactAction.performed += GetInputs;
+        panAction.performed += GetInputs;
+
+    }
+
+    private void GetInputs(InputAction.CallbackContext context)
+    {
+        Debug.Log(context.action);
+        if (context.action == lookAction)
+        {
+            Vector2 direction = context.ReadValue<Vector2>();
+            Debug.Log(direction);
+            enCameraPanDirections thisDirection = enCameraPanDirections.none;
+            if (direction.x == 1)
+            {
+                thisDirection = enCameraPanDirections.right;
+            }
+            else if (direction.x == -1)
+            {
+                thisDirection = enCameraPanDirections.left;
+            }
+            else if (direction.y == 1)
+            {
+                thisDirection = enCameraPanDirections.up;
+            }
+            else if (direction.y == -1)
+            {
+                thisDirection = enCameraPanDirections.down;
+            }
+
+            if (thisDirection != enCameraPanDirections.none)
+            {
+                cameraControl.PanCamera(thisDirection);
+            }
+        }
+        if (context.action == interactAction) { }
+        if (context.action == panAction) { }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
