83768fac8ebb1852422dcca459e5234a1669d623
gregf <115318076+gregf@users.noreply.github.com>
2026-03-03 12:41:11 +0000

    Revert "test"
    This reverts commit 3fb8f5e009f76a85c48087ce0dc1acf7f28d5b87.

---
+++ FLAG_Bowling_game/Assets/Scripts/Menus_Language/Main menu.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Mainmenu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
