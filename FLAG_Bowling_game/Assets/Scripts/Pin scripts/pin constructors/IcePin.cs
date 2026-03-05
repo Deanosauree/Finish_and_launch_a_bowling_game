c5f4253b1c5a7df7d7b47240e29bcd6ab77cbe83
gregf <115318076+gregf@users.noreply.github.com>
2026-03-05 17:03:52 +0000

    new pins

---
+++ FLAG_Bowling_game/Assets/Scripts/Pin scripts/pin constructors/IcePin.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class IcePin : PinBase
+{
+    public IcePin()//constractor
+    {
+        weight = 10;
+    }
+}
---
+++ FLAG_Bowling_game/Assets/Scripts/Pin scripts/pin constructors/TungstenPin.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class TungstenPin : PinBase
+{
+    public TungstenPin()//constractor
+    {
+        points = 100;
+        weight = 30;
+        GMultiplier = 1.2f;
+    }
+}
