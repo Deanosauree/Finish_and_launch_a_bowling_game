2734848ea4cdc6b6d004f817dffa29ddc5d42921
gregf <115318076+gregf@users.noreply.github.com>
2026-03-08 22:23:44 +0000

    main menu settings implemented (volume and graphics presets need to be set)

--- FLAG_Bowling_game/Assets/Scripts/Menus_Language/Main menu.cs
+++ FLAG_Bowling_game/Assets/Scripts/Menus_Language/Main menu.cs
@@ -1,8 +1,45 @@
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Mainmenu : MonoBehaviour
 {
+
+    Resolution[] resolutions;
+    public TMP_Dropdown resolutionDropdown;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+
+    public void SetResolution(int ResolutionIndex)
+    {
+        Resolution resolution = resolutions[ResolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
     public void Play()
     {
         SceneManager.LoadSceneAsync(1);
@@ -10,5 +47,20 @@ public class Mainmenu : MonoBehaviour
     public void Quit()
     {
         Application.Quit();
+    }
+
+    public void SetVolume(float volume)
+    {
+        Debug.Log(volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void setFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
     }
 }
